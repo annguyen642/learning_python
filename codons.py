acee80f16e58caad2150c211eb03316616bd569d
annguyen642 <annguyen1015@gmail.com>
2021-04-09 23:23:19 +0000

    Week 2

--- codons.py
+++ codons.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+
+# Print out all the codons for the sequence below in reading frame 1
+# Use a 'for' loop
+
+
+
 
 # your code goes here
 dna = 'ATAGCGAATATCTCTCATGAGAGGGAA'
